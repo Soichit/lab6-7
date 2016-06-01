0c56348de1820dd9dc0363e063158fedf5b233e4
Soichi Tanabe <soichit@D-172-28-36-74.dhcp4.washington.edu>
2016-06-01 06:06:57 -0700

    UI upgrades

--- static/main.js
+++ static/main.js
@@ -1,4 +1,15 @@
 $(function(){
+
+    var jsonData;
+    var jsonName;
+    var jsonUrl;
+    var index;
+
+    window.onload = function() {
+        document.getElementById("dislike").onclick = disliked;
+        document.getElementById("like").onclick = liked;
+    };
+
     $.get("/ping", function(data){
         if(data.error == "true"){
             $("#results").prepend("<div class='alert alert-danger'><strong>Error!</strong> "+ data.message +"</div>");
@@ -7,16 +18,35 @@ $(function(){
 
     $.get("getFoodStack", function(data){
         console.log(data);
-
-        var name = data.result[0][2];
-        var url = data.result[0][1];
+        jsonData = data;
+        jsonName = data.result[0][2];
+        jsonUrl = data.result[0][1];
         console.log(name);
-
-
-        document.getElementById("foodName").innerHTML = name;
-        document.getElementById("foodImage").src = url;
-
-
+        document.getElementById("foodName").innerHTML = jsonName;
+        document.getElementById("foodImage").src = jsonUrl;
     }, "json")
 
+    function getData(i) {
+        jsonName = data.result[i][2];
+        jsonUrl = data.result[i][1];
+    }
+
+
+
+
+    function disliked() {
+        console.log("DISLIKE");
+    }
+
+    function liked() {
+        console.log("LIKE");
+        document.getElementById("youMatched").innerHTML = "It's a Match! ...with ";
+    }
+
+
 })
+
+
+
+
+
