3ab113ec67bb8ba4b94d6f86f0353a93c42e8128
Soichi Tanabe <soichit@D-172-28-36-74.dhcp4.washington.edu>
2016-06-01 10:02:16 -0700

    UX changes

--- html/main.js
+++ html/main.js
@@ -13,16 +13,32 @@ $(function(){
         [10, 'https://images3.alphacoders.com/248/248457.jpg', 'tacos']
     ]};
 
+    var restaurantData={'result':[
+        [1, 'https://cache.dominos.com/olo/3_19_0/assets/build/images/promo/dominos_social_logo.jpg'],
+        [2, 'http://www.johnscaffe.com/wp-content/uploads/2016/03/slider-Johns-Caffe-Pizza.jpg'],
+        [3, 'http://media.cuponofertas.com.mx/2014/02/dominos-pizzas-oferta.jpg'],
+        [4, 'http://www.recipemash.com/wp-content/uploads/2013/01/How-to-Make-Dominos-Lava-Cake-.jpg']
+    ]};
+
 
 
     var jsonName;
     var jsonUrl;
     var index = 0;
 
+    var restaurantIndex = 0;
+    var restaurantView = false;
+
     window.onload = function() {
         document.getElementById("dislike").onclick = disliked;
         document.getElementById("like").onclick = liked;
+        document.getElementById("foodImage").onclick = clickedImage;
+
+        document.getElementById("rightArrow").onclick = clickedRight;
+        document.getElementById("leftArrow").onclick = clickedLeft;
+        document.getElementById("backButton").onclick = clickedBack;
         getData();
+        hideElements();
     };
 
     $.get("/ping", function(data){
@@ -44,22 +60,82 @@ $(function(){
     */
 
     function getData() {
-        jsonName = jsonData.result[index][2];
-        jsonUrl = jsonData.result[index][1];
+        if (restaurantView) {
+            jsonUrl = restaurantData.result[index][1];
+        } else {
+            jsonUrl = jsonData.result[index][1];
+            jsonName = jsonData.result[index][2];
+        }
+        document.getElementById("foodName").innerHTML = jsonName;
         document.getElementById("foodImage").src = jsonUrl;
+       
     }
 
 
+    function clickedImage() {
+        if (index == 0) {
+            restaurantIndex = 0;
+            restaurantView = !restaurantView;
+            setRestaurantImage();
+        }
+    }
 
+    function clickedRight() {
+        restaurantIndex++;
+        console.log(restaurantIndex);
+        setRestaurantImage();
+    }
+
+    function clickedLeft() {
+        restaurantIndex--;
+        console.log(restaurantIndex);
+        setRestaurantImage();
+    }
+
+    function setRestaurantImage() {
+        if (restaurantView) {
+            document.getElementById("backButton").style.visibility = "visible";
+            document.getElementById("leftArrow").style.visibility = "visible";
+            document.getElementById("rightArrow").style.visibility = "visible";
+        } else {
+            hideElements();
+        }
+        jsonUrl = restaurantData.result[restaurantIndex][1];
+        jsonName = "Domino's Pizza";
+        document.getElementById("foodName").innerHTML = jsonName;
+        document.getElementById("foodImage").src = jsonUrl;
+    }
 
     function disliked() {
-        document.getElementById("youMatched").innerHTML = "";
+        if (index >= 9) {
+            index = -1;
+        }
         index++;
-        getData()
+        restaurantIndex = 0;
+        document.getElementById("youMatched").innerHTML = "";
+        restaurantView = false;
+        getData();
+        hideElements();
     }
 
     function liked() {
         document.getElementById("youMatched").innerHTML = "It's a Match! ...with " + jsonName;
+        restaurantView = false;
+        hideElements();
+    }
+
+    function clickedBack() {
+        restaurantIndex = 0;
+        document.getElementById("youMatched").innerHTML = "";
+        restaurantView = false;
+        getData();
+        hideElements();
+    }
+
+    function hideElements() {
+        document.getElementById("backButton").style.visibility = "hidden";
+        document.getElementById("leftArrow").style.visibility = "hidden";
+        document.getElementById("rightArrow").style.visibility = "hidden";
     }
 
 
