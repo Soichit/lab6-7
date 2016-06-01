c30fead803dd742c4a3b80ff6704dd44629e1884
Soichi Tanabe <soichit@D-172-28-36-74.dhcp4.washington.edu>
2016-06-01 01:13:02 -0700

    a

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -86,7 +86,7 @@ func main() {
 			// preface each variable with &
 			rows.Scan(&id, &image, &name, &price) // <--- EDIT THIS LINE
 			// can't combine ints and strings in Go. Use strconv.Itoa(int) instead
-			table += "<tr><td>" + strconv.Itoa(id) + "</td><td>" + image + "</td><td>" + name + "</td></tr>" + price + "</td></tr>" // <--- EDIT THIS LINE
+			table += "<tr><td>" + strconv.Itoa(id) + "</td><td>" + image + "</td><td>" + name + "</td></tr>" + strconv.FormatFloat(price) + "</td></tr>" // <--- EDIT THIS LINE
 		}
 		// finally, close out the body and table
 		table += "</tbody></table>"
