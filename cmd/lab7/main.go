a14281a47e74f86c424731cfa21b02f9be2cc85c
Soichi Tanabe <soichit@D-172-28-36-74.dhcp4.washington.edu>
2016-06-01 00:21:56 -0700

    a

--- cmd/lab7/main.go
+++ cmd/lab7/main.go
@@ -91,7 +91,7 @@ func main() {
 	router.GET("/query2", func(c *gin.Context) {
 		table := "<table class='table'><thead><tr>"
 		// put your query here
-		rows, err := db.Query("SELECT FROM class") // <--- EDIT THIS LINE
+		rows, err := db.Query("SELECT * FROM class") // <--- EDIT THIS LINE
 		if err != nil {
 			// careful about returning errors to the user!
 			c.AbortWithError(http.StatusInternalServerError, err)
